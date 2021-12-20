00f9a2bbaeb5c519e492739d7aa135382a75f47c
本馬遼太郎 <rajiku.blanc@gmail.com>
2021-12-20 11:54:02 +0900

    Header/Footer

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,14 @@
+import Footer from './Footer';
+import Header from './Header';
+
 const Layout = ({ children }: any) => {
   return (
     <>
-      <main className='max-h-screen max-w-full flex justify-center items-center'>
+      <Header />
+      <main className='min-h-screen max-w-full flex justify-center items-center'>
         {children}
       </main>
+      <Footer />
     </>
   );
 };
